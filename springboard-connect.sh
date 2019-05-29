be5ca36c3f5dfe5b5f0bd651c7d4327547d32c81
Rahul Lad <rahullad@unscrambl.com>
2019-05-29 18:31:57 +0530

    config updated battery status warning added

---
+++ springboard-connect.sh
@@ -0,0 +1,13 @@
+curl -X POST \
+  http://portal.91springboard.com/login \
+  -H 'Accept: */*' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Host: portal.91springboard.com' \
+  -H 'Postman-Token: b1da3d49-0e3f-4220-9b36-c587ec7017f5,3e4436f3-67c4-427c-9238-307b5023dfca' \
+  -H 'User-Agent: PostmanRuntime/7.11.0' \
+  -H 'accept-encoding: gzip, deflate' \
+  -H 'cache-control: no-cache' \
+  -H 'content-length: 105' \
+  -d 'dst=https%3A%2F%2Fevents.91springboard.com&popup=true&username=ladrahul00%40gmail.com&password=l%40sth0pe'
